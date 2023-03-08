d1cab2e15dd45ce24db1bd6663a5ef57305e24aa
AelinaXY <Lauren@2infinity.net>
2023-03-08 11:36:38 +0000

    Answers to question 5

--- sakila answers.sql
+++ sakila answers.sql
@@ -4,12 +4,13 @@ USE sakila;
 --  INNER JOIN actor ON film_actor.actor_id = actor.actor_id 
 --  GROUP BY film_actor.actor_id ORDER BY COUNT(film_actor.actor_id) DESC LIMIT 1;
 --  
---  select avg(film.length) as 'Average Film Length' from	film;
+--  select avg(film.length) as 'Average Film Length' from film;
 --  
 -- select category.name as 'Category Name', avg(film.length) as 'Average Length' from film
 -- 	inner join film_category on film.film_id = film_category.film_id
 --     inner join category on film_category.category_id = category.category_id
--- 	group by film_category.category_id order by avg(film.length) DESC;
+-- 	group by film_category.category_id order by avg(film.length) desc;
 
 -- select count(film.film_id) as "Film's with Robots" from film where `description` like '%robot%';
 
+-- select film.title as 'Title', film.length as 'Length' from film where film.length = (select max(film.length) from film);
