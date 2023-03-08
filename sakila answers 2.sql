4ba65acfa0e207b0e4cd62d60f182755d760506c
AelinaXY <Lauren@2infinity.net>
2023-03-08 14:58:36 +0000

    Add answers to sakila exercise 2 question 1

---
+++ sakila answers 2.sql
@@ -0,0 +1,19 @@
+use sakila;
+
+-- EXERCISES
+
+-- What is the average running time of films by category? 
+
+-- Which last names appear more than once? 
+
+-- Retrieve all movies that Fred Costner has appeared in. 
+
+-- Find out which location has the most copies of BUCKET BROTHERHOOD. 
+
+-- Create a list of categories and the number of films for each category. 
+
+-- Create a list of actors and the number of movies by each actor. 
+
+-- Is ‘Academy Dinosaur’ available for rent from Store 1? 
+
+-- When is ‘Academy Dinosaur’ due? 
